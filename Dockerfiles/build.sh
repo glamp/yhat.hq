037801504d82feeb32cbd58f9f2579b49c08edef
Greg Lamp <lamp.greg@gmail.com>
2013-09-16 18:11:11 -0400

    blah

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+docker build -t node.v0.10.17 https://raw.github.com/glamp/yhat.hq/master/Dockerfiles/node.v0.10.17
+docker build -t scipy-and-r https://raw.github.com/glamp/yhat.hq/master/Dockerfiles/scipy-and-r
